5ebc32f1f2791f648e968c8e838ad641ffb6f734
hui00 <kevinbrinkschulte@hotmail.de>
2022-11-01 23:30:10 +0100

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\w+\z/ }
+end
